40a40315b76080fe1e2d6895c8912c05bbc2f479
forente <zerofox16@gmail.com>
2018-08-11 12:34:56 -0400

    Pick up

---
+++ LudemDare42/Assets/_Scripts/PickPassangers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickPassangers : MonoBehaviour {
+
+	public int maxPassengerCount = 6; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Destroy(other.gameObject);
+	}
+}
